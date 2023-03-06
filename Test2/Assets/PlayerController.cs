281c5668de13bc8b9bfa9a86d73108ebe6d65a16
huni9942 <huni9942@naver.com>
2023-03-06 13:55:18 +0900

    Test2

---
+++ Test2/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float Speed;
+
+
+    void Start()
+    {
+        Speed = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Hor = Input.GetAxis("Horizontal");
+        float Ver = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(
+            Hor*Time.deltaTime*Speed, Ver * Time.deltaTime * Speed, 0.0f);
+
+    }
+}
