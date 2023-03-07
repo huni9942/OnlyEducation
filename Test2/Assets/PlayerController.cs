c4c4e7a46af24c40b7f7e67a7ce3f46791ae7f7e
huni9942 <huni9942@naver.com>
2023-03-07 13:50:38 +0900

    test307

--- Test2/Assets/PlayerController.cs
+++ Test2/Assets/PlayerController.cs
@@ -2,26 +2,173 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// [2의 보수]
+// 0 0000000 
+//    0 0000001 = 1
+// - 0 0000010 = 2
+//--------------
+// 1 - 2 = 1 + (-2)
+// 1 + (-2)
+
+//    0 0000001 = 1
+// + 11111110 = (-2)
+//--------------
+//    11111111 = (-1)
+//    00000001 = 1
+
+
+// 0 0000100 = 4
+// 0 0001000 = 8
+// 0 0010000 = 16
+// 0 0100000 = 32
+// 0 1000000 = 64
+// 1 1111111 =  128
+
+
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-
+    //  움직이는 속도
     private float Speed;
+    private Vector3 Movement;
+
+    private Animator animator;
+
+    private bool onAttack;
+    private bool onHit;
+    private bool onRoll;
+    private bool onJump;
+    private bool onDive;
 
 
+    //  유니티 기본 제공 함수
+    //  초기값을 설정할 때 사용
     void Start()
     {
+        //  속도를 초기화.
         Speed = 5.0f;
+
+        //  player 의 Animator를 받아온다.
+        animator = this.GetComponent<Animator>();
+
+        onAttack = false;
+
+        onHit = false;
+
+        onRoll = false;
+
+        onJump = false;
+
+        onDive = false;
     }
 
-    // Update is called once per frame
+    //  유니티 기본 제공 함수
+    //  프레임마다 반복적으로 실행되는 함수.
     void Update()
     {
-        float Hor = Input.GetAxis("Horizontal");
-        float Ver = Input.GetAxis("Vertical");
+        //  [실수 연산 IEEE754]
 
-        transform.position += new Vector3(
-            Hor*Time.deltaTime*Speed, Ver * Time.deltaTime * Speed, 0.0f);
+        // **  Input.GetAxis =     -1 ~ 1 사이의 값을 반환함. 
+        float Hor = Input.GetAxisRaw("Horizontal"); // -1 or 0 or 1 셋중에 하나를 반환.
+        float Ver = Input.GetAxis("Vertical"); // -1 ~ 1 까지 실수로 반환.
 
+        Movement = new Vector3(
+            Hor * Time.deltaTime * Speed,
+            Ver * Time.deltaTime * Speed,
+            0.0f);
+
+        if (Input.GetKey(KeyCode.LeftControl))
+            OnAttack();
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            OnHit();
+
+        if (Input.GetKey(KeyCode.RightShift))
+            OnRoll();
+
+        if (Input.GetKey(KeyCode.Space))
+            OnJump();
+
+        if (Input.GetKey(KeyCode.LeftAlt))
+            OnDive();
+
+        animator.SetFloat("Speed", Hor);
+        transform.position += Movement;
+    }
+
+
+    private void OnAttack()
+    {
+        if (onAttack)
+            return;
+
+        onAttack = true;
+        animator.SetTrigger("Attack");
+    }
+
+    private void SetAttack()
+    {
+        onAttack = false;
+    }
+
+
+    private void OnHit()
+    {
+        if (onHit)
+            return;
+
+        onHit = true;
+        animator.SetTrigger("Hit");
+    }
+
+    private void SetHit()
+    {
+        onHit = false;
+    }
+
+
+    private void OnRoll()
+    {
+        if (onRoll)
+            return;
+
+        onRoll = true;
+        animator.SetTrigger("Roll");
+    }
+
+
+    private void SetRoll()
+    {
+        onRoll = false;
+    }
+
+    private void OnJump()
+    {
+        if (onJump)
+            return;
+
+        onJump = true;
+        animator.SetTrigger("Jump");
+    }
+
+
+    private void SetJump()
+    {
+        onJump = false;
+    }
+
+    private void OnDive()
+    {
+        if (onDive)
+            return;
+
+        onJump = true;
+        animator.SetTrigger("Dive");
+    }
+
+
+    private void SetDive()
+    {
+        onDive = false;
     }
 }
+
